76c9980b1392e605e27e02dfd7ab223f15c384e1
Moha18lrid <91798947+Moha18lrid@users.noreply.github.com>
2023-12-12 16:37:54 +0100

    Add files via upload

---
+++ editor.js
@@ -0,0 +1,68 @@
+let setrurate = document.getElementById("saturate");
+let contrast = document.getElementById("contrast");
+let brightness = document.getElementById("brightness");
+let sepia = document.getElementById("sepia");
+let grayscale = document.getElementById("grayscale");
+let blur = document.getElementById("blur");
+let huerotate = document.getElementById("hue-rotate");
+let upload = document.getElementById("input");
+let download = document.getElementById("download");
+let img = document.getElementById("img");
+let reset = document.querySelector('span');
+let imgbox = document.querySelector('.imgbox');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+function restvalue() {
+    ctx.filter = 'none';
+    ctx.drawImage(img,0,0,canvas.width,canvas.height);
+    setrurate.value = '100';
+    contrast.value = '100';
+    brightness.value = '100';
+    sepia.value = '0';
+    grayscale.value = '0';
+    blur.value = '0';
+    huerotate.value = '0';
+
+}
+
+window.onload = function () {
+    download.style.display = 'none';
+    reset.style.display = 'none';
+    imgbox.style.display = 'none';
+}
+upload.onchange = function () {
+    restvalue();
+    download.style.display = 'block';
+    reset.style.display = 'block';
+    imgbox.style.display = 'block';
+    let file = new FileReader();
+    file.readAsDataURL(upload.files[0]);
+    file.onload = function () {
+        img.src = file.result;
+    }
+    img.onload = function () {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img,0,0,canvas.width,canvas.height);
+        img.style.display = 'none';
+    }
+}
+let filters = document.querySelectorAll("ul li input");
+filters.forEach(filer => {
+    filer.addEventListener('input', function () {
+        ctx.filter = `
+        saturate(${setrurate.value}%)
+        contrast(${contrast.value}%)
+        brightness(${brightness.value}%)
+        sepia(${sepia.value}%)
+        grayscale(${grayscale.value})
+        blur(${blur.value}px)
+        hue-rotate(${huerotate.value}deg)
+        `
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    })
+})
+
+download.onclick = function () {
+    download.href = canvas.toDataURL('image/jpeg');
+}
